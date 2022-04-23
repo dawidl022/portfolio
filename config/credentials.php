19adbef8cbeb8d2f878156fcf5c3f951d16670fc
Dawid Lachowicz <domdaw7@gmail.com>
2022-04-23 22:03:41 +0100

    Update env variable names

--- config/credentials.php
+++ config/credentials.php
@@ -1,6 +1,6 @@
 <?php
   define('DB_HOST', '127.0.0.1');
-  define('DB_USER', getenv('MYSQL_USER'));
-  define('DB_PASS', getenv('MYSQL_PASSWORD'));
-  define('DB_NAME', getenv('MYSQL_DATABASE'));
+  define('DB_USER', getenv('database-user'));
+  define('DB_PASS', getenv('database-password'));
+  define('DB_NAME', getenv('database-name'));
 ?>
