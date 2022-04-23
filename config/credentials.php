066a337ff6eb2689860fb35b241432c5e56c1028
Dawid Lachowicz <domdaw7@gmail.com>
2022-04-23 21:32:49 +0100

    Change localhost to loopback address

--- config/credentials.php
+++ config/credentials.php
@@ -1,5 +1,5 @@
 <?php
-  define('DB_HOST', 'localhost');
+  define('DB_HOST', '127.0.0.1');
   define('DB_USER', getenv('MYSQL_USER'));
   define('DB_PASS', getenv('MYSQL_PASSWORD'));
   define('DB_NAME', getenv('MYSQL_DATABASE'));
