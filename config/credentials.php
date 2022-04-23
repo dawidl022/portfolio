e36a0d6dd839db53742f9806c404a0520fd3f852
Dawid Lachowicz <domdaw7@gmail.com>
2022-04-23 21:22:38 +0100

    Set config to use environment variables

---
+++ config/credentials.php
@@ -0,0 +1,6 @@
+<?php
+  define('DB_HOST', 'localhost');
+  define('DB_USER', getenv('MYSQL_USER'));
+  define('DB_PASS', getenv('MYSQL_PASSWORD'));
+  define('DB_NAME', getenv('MYSQL_DATABASE'));
+?>
